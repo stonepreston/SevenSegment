10525848154be324f1aca7693aeb1a581a997e05
Stone Preston <stone.connor.preston@gmail.com>
2017-02-28 13:33:07 -0600

    updated example

--- Examples/SevenSegment.ino
+++ Examples/SevenSegment.ino
@@ -1,12 +1,26 @@
 #include "Arduino.h"
+#include <SevenSegment.h>
+
+// Create SevenSegment object using default pin configuration
+SevenSegment seg = SevenSegment();
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+	int dt = 1;
+	seg.display(1, 'c', false);
+	delay(dt);
+	seg.display(2, 'o', false);
+	delay(dt);
+	seg.display(3, 'o', false);
+	delay(dt);
+	seg.display(4, 'l', false);
+	delay(dt);
+
 }
